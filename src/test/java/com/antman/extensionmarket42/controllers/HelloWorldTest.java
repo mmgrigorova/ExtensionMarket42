6d77f1efcc6b95d2ec34a0bfb2be6f4ee9eccb91
Maria Grigorova <mmgrigorova@gmail.com>
2018-08-16 16:27:17 +0300

    Add Hello World unit test

---
+++ src/test/java/com/antman/extensionmarket42/controllers/HelloWorldTest.java
@@ -0,0 +1,36 @@
+package com.antman.extensionmarket42.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloWorldTest {
+    private String hello;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void helloWorld_whenHello_shouldStatus200() throws Exception {
+        ResultActions expect = mockMvc.perform(
+                get("/")
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        expect.andExpect(content().string("Hello World"));
+    }
+}
