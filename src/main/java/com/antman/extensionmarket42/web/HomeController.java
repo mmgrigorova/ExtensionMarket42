d50a1b58bfd0c43872e4b2b4f972568de9cff803
Maria Grigorova <mmgrigorova@gmail.com>
2018-08-19 23:41:44 +0300

    Show authorities and username of logged user

--- src/main/java/com/antman/extensionmarket42/web/HomeController.java
+++ src/main/java/com/antman/extensionmarket42/web/HomeController.java
@@ -1,12 +1,25 @@
 package com.antman.extensionmarket42.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String showHome(){
-        return "index";
+    public ModelAndView showHome(Authentication authentication){
+        ModelAndView mav = new ModelAndView("index");
+        UserDetails userDetails;
+
+        if (authentication != null){
+            userDetails = (UserDetails) authentication.getPrincipal();
+            System.out.println("User has authorities: " + userDetails.getAuthorities());
+            return mav.addObject("username", userDetails.getUsername());
+        }
+
+        return mav;
     }
 }
