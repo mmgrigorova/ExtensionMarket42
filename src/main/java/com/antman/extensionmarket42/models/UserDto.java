2cf12535d39e336f809d4c7835121bbfefce0b56
Maria Grigorova <mmgrigorova@gmail.com>
2018-08-20 12:26:56 +0300

    Added name size message

--- src/main/java/com/antman/extensionmarket42/models/UserDto.java
+++ src/main/java/com/antman/extensionmarket42/models/UserDto.java
@@ -3,16 +3,20 @@ package com.antman.extensionmarket42.models;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class UserDto {
     private static final String REQUIRED_MESSAGE = "This field is required";
+    private static final String NAME_SIZE_MESSAGE = "Name should be at least two characters long";
 
     @NotNull
     @NotEmpty(message = REQUIRED_MESSAGE)
+    @Size(min = 2, message = NAME_SIZE_MESSAGE)
     private String firstname;
 
     @NotNull
     @NotEmpty(message = REQUIRED_MESSAGE)
+    @Size(min = 2, message = NAME_SIZE_MESSAGE)
     private String lastname;
 
     @NotNull
