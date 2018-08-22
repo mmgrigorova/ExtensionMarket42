d12b76bd08c0e6cbba80f56b672baa533b55a47d
Maria Grigorova <mmgrigorova@gmail.com>
2018-08-22 18:33:42 +0300

    Upload file - jQuery and REST

--- src/main/java/com/antman/extensionmarket42/webcontrollers/users/developers/ExtensionController.java
+++ src/main/java/com/antman/extensionmarket42/webcontrollers/users/developers/ExtensionController.java
@@ -2,9 +2,12 @@ package com.antman.extensionmarket42.webcontrollers.users.developers;
 
 import com.antman.extensionmarket42.services.extensions.ExtensionService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller()
 public class ExtensionController {
-
-
+    @GetMapping("/extension-add")
+    public String showAddExtension(){
+        return "extension-add";
+    }
 }
