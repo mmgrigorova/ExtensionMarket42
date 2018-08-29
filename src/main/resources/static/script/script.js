feb135be48da0b95bdfebd7d27f924836534049e
Maria Grigorova <mmgrigorova@gmail.com>
2018-08-29 20:02:56 +0300

    Add API for available tags

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -4,5 +4,16 @@ $(document).ready(function () {
         var menuId = $('h1').attr('id');
         $('#' + menuId).addClass("active");
         console.log("doing something");
+
+        $('#textarea').textext({
+            plugins: 'tags prompt focus autocomplete ajax arrow',
+            tagsItems: ['Basic', 'JavaScript', 'PHP', 'Scala'],
+            prompt: 'Add one...',
+            ajax: {
+                url: '/manual/examples/data.json',
+                dataType: 'json',
+                cacheResults: true
+            }
+        });
     }
 );
