00013d9e0743de96f548a590fc61dedd04a67df0
Maria Grigorova <mmgrigorova@gmail.com>
2018-09-02 19:00:32 +0300

    Extend SpringBootServletInitializer to support deployable wars

--- src/main/java/com/antman/extensionmarket42/Extensionmarket42Application.java
+++ src/main/java/com/antman/extensionmarket42/Extensionmarket42Application.java
@@ -1,15 +1,23 @@
 package com.antman.extensionmarket42;
 
 import com.antman.extensionmarket42.utils.FileStorageProperties;
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
         FileStorageProperties.class
 })
-public class Extensionmarket42Application {
+public class Extensionmarket42Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Extensionmarket42Application.class);
+    }
 
     public static void main(String[] args) {
        SpringApplication.run(Extensionmarket42Application.class, args);
