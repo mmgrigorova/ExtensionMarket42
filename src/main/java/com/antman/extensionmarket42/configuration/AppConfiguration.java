2802f5cd730149b665ff5a22c5f08c741a02a237
Maria Grigorova <mmgrigorova@gmail.com>
2018-09-05 16:01:01 +0300

    Change settings for deployment

--- src/main/java/com/antman/extensionmarket42/configuration/AppConfiguration.java
+++ src/main/java/com/antman/extensionmarket42/configuration/AppConfiguration.java
@@ -36,18 +36,18 @@ public class AppConfiguration {
     }
 
 
-    @Bean
-    public SessionFactory createSessionFactory() {
-        return new org.hibernate.cfg.Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Extension.class)
-                .addAnnotatedClass(Screenshot.class)
-                .addAnnotatedClass(Tag.class)
-                .addAnnotatedClass(User.class)
-                .addAnnotatedClass(UserProfile.class)
-                .addAnnotatedClass(UserRole.class)
-                .buildSessionFactory();
-    }
+//    @Bean
+//    public SessionFactory createSessionFactory() {
+//        return new org.hibernate.cfg.Configuration()
+//                .configure("hibernate.cfg.xml")
+//                .addAnnotatedClass(Extension.class)
+//                .addAnnotatedClass(Screenshot.class)
+//                .addAnnotatedClass(Tag.class)
+//                .addAnnotatedClass(User.class)
+//                .addAnnotatedClass(UserProfile.class)
+//                .addAnnotatedClass(UserRole.class)
+//                .buildSessionFactory();
+//    }
 
     @Bean
     public DataSource securityDataSource(){
