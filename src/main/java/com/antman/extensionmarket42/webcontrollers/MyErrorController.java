a66fac855f94e3b81fdeec96a59e07828733325e
Maria Grigorova <mmgrigorova@gmail.com>
2018-09-06 17:29:50 +0300

    Style Extension details page

--- src/main/java/com/antman/extensionmarket42/webcontrollers/MyErrorController.java
+++ src/main/java/com/antman/extensionmarket42/webcontrollers/MyErrorController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
